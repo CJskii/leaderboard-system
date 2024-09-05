16c70f6a76e85859c741e43d6d41912009f942c9
Bukowa <gitbukowa@gmail.com>
2024-09-05 20:32:11 +0200

    chore: use sqlite as default database

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
